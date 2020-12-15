ba13b6eb19b8ee17ccf0fcfa7f4da194ad2b9771
fafargamer <68151749+fafargamer@users.noreply.github.com>
2020-12-15 18:58:05 +0700

    views update

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
